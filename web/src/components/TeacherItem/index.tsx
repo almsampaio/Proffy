084594a86794ff7c978815f02df27b31edbba620
Andre Sampaio <andresampaiolm@gmail.com>
2020-08-07 00:30:08 -0300

    api_connected_to_web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,32 +4,50 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+import api from '../../services/api'
+
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
                 <article className="teacher-item">
                     <header>
-                        <img src="https://avatars0.githubusercontent.com/u/45184938?s=460&u=45813fb1b170eba7eaffbfcb6c57b06c6523c495&v=4" alt="Andre Sampaio"/>
+                        <img src={teacher.avatar} alt={teacher.name} />
                         <div>
-                            <strong>Andre Sampaio</strong>
-                            <span>Matemática</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a imperdiet lorem. Pellentesque lacinia sem in sapien pellentesque pretium. 
-                        <br/> <br/>
-                        Praesent ante ligula, consectetur quis nisi dignissim, hendrerit viverra lectus. Aenean ut facilisis odio, efficitur rutrum ipsum.
-                    </p>
+                    <p>{teacher.bio}</p>
 
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$ 45,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                             <img src={whatsappIcon} alt="Whatsapp"/>
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     )
